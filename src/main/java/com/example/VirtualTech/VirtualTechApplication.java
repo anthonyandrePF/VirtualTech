92c82035344c516f097bf1af891d38eaeddf9613
Anthony PF <aapf672@gmail.com>
2025-08-14 18:08:06 -0500

    nueva pagina

---
+++ src/main/java/com/example/VirtualTech/VirtualTechApplication.java
@@ -0,0 +1,14 @@
+package com.example.VirtualTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualTechApplication.class, args);
+	}
+
+}
+	
---
+++ src/test/java/com/example/VirtualTech/VirtualTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VirtualTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
